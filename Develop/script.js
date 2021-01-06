d85e6889b30283696ebe3a24881b8d5c2268e008
MWPX777 <mwpx777@gmail.com>
2021-01-06 16:36:45 -0600

    save before tutor

--- Develop/script.js
+++ Develop/script.js
@@ -1,58 +1,54 @@
-//let timeRightNow = moment().format('h');
+let newTask = document.querySelector('newTask');
+let hourNumber = document.querySelector('.number').innerHTML;
+let timeValue = moment().set('hour');
+let scheduleHourNumber = parseInt(hourNumber);
+console.log("schedule hour number is " , scheduleHourNumber);
 
-const newTask = document.querySelector('newTask');
 
-
-//console.log(typeof(timeValue))
-//var saveTasks = function() {
-    // localStorage.setItem("tasks", JSON.stringify(tasks));
-    //};
-    
-  
-
-  
-    
 //displayDate Function
 function displayDate() {
     
-let currentDate = moment().format('MMMM Do YYYY, h:mm:ss A');
-document.querySelector(".date").innerHTML = currentDate;
+    let currentDate = moment().format('MMMM Do YYYY, h:mm:ss A');
+    document.querySelector(".date").innerHTML = currentDate;
 } 
-        //this calls the displayDate function every 1000ms
+//this calls the displayDate function every 1000ms
 setInterval (displayDate, 1000);
 
 //this will run 'displayDate' when page is loaded
 displayDate();
-    
-    
+
+
 //currentTime Function
 function currentTime(){
-let timeValue = moment().set('hour')
-//let timeValue = $(taskEl).find('span').text();
-//console.log("time", timeValue);
-//let timeRightNow = moment().format('h');
 
-//if (timeRightNow == timeValue){
+    //if timeValue is past 12PM, the number will be subtracted by 12
+    if(timeValue >= 13){
+        timeValue = timeValue - 12
+        console.log(timeValue);
+    };   
 
-                                                //this area not working
-if(moment().isBefore(timeValue)){
-   
-    console.log(timeValue);
-    
-    $('.centerCol').css("background-color","grey")
-    }
-
-    else if(moment() == timeValue) {
-    //$('.centerCol').css("background-color","red")
-
-    }
+    changeBackground();
 }
 
-//this runs the currentTime function when page loads
-currentTime();
-   
+function changeBackground(){
+    //for(var i=0; i> )                              //would I need for loop here?
+    if (timeValue == scheduleHourNumber){
+        $('.color').css("background-color", "lightred")
+        console.log('it matches');                   //this function is not working
+    }
+    /*else{
+        $('.color').css("background-color", "lightgreen")
+       console.log("it doesnt match");
+    }*/
+}
+//this will run 'changeBackground every 1 minute
+setInterval (changeBackground, 60000);
+
 createNewTask = function(){
-    var task = document.createElement('input')
+    
+    //this will create <input> in HTML
+    //var task = $('<input>')
+    var task = document.createElement('input')   //this only works on 9:00AM
     //.addClass('form-control')
     newTask.append(task);
     console.log(task);
@@ -72,8 +68,9 @@ deleteTask = function(){
 saveTask = function(){
     //this is variable of value that retrieves the user 'input' from input box
     var value = $('input').val();
+    
     //this should set the value of 'var value' to local storage with 'userInput' label
-    localStorage.setItem('userInput', value)
+    localStorage.setItem('userInput', JSON.stringify(value))
     
     console.log(value);
      
@@ -88,18 +85,22 @@ enterText = function(){
     
 }
 
-//task is clicked off of
+// save task  when text box is clicked off of
 $('newTask').on('blur', 'input',  function(){
-    saveTask1();
+    saveTask();
     console.log('saveTask')
 });
 
 
 //loadTasks Function
-loadTasks = function(){
+loadTasks = function(){                         //this function not working
     task = localStorage.getItem('userInput');
     //input = 
 }
+
+
+//this runs the currentTime function when page loads
+currentTime();
 
 //this will load tasks from local storage
 loadTasks();
@@ -110,16 +111,16 @@ $('newTask').click(enterText);
 //button click events
 $('#newBtn1').click(createNewTask);
 
+
 $('#deleteBtn1').click(deleteTask);
 
 $('#saveBtn1').click(saveTask);
 
 
 
-  //compare current time to time on schedule  
- //set background color of past time slots to grey
+   
+ //set background color of future time slots to green
  //set background color of current time slots to red
  
  
 //load items into schedule from local storage if page is refreshed
-//refresh curentTime every 1 minute
